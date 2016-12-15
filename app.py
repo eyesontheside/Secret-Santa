2ec21f9d03ea8721ed112cfee4efc1924cede95c
Spencer Harper <hjw848@vols.utk.edu>
2016-12-15 13:51:07 -0500

    Login html and css added

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return '<h1>Hello World!</h1>'
+
+@app.route('/login/')
+def login():
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
